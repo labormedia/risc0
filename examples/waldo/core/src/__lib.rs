285e57a041564dcafad84a1cefa52e7ef364fc93
Diego Correa T <algoritmia@labormedia.cl>
2023-04-30 22:45:26 +0000

    pruning

---
+++ examples/waldo/core/src/__lib.rs
@@ -0,0 +1,12 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+// use serde::{Deserialize, Serialize};
+// use serde::__private::Vec;
+
+use serde;
+use bytemuck;
+use divrem;
+// use elsa;
+use merkle_light;
+use merkle_light_derive;
+// use risc0_zkvm;
