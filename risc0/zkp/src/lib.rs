8702678752744be08943395f7b06abe62c1b8ba0
Diego Correa Tristain <algoritmia@labormedia.cl>
2023-05-26 22:24:42 +0000

    fix alloc

--- risc0/zkp/src/lib.rs
+++ risc0/zkp/src/lib.rs
@@ -16,7 +16,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![deny(rustdoc::broken_intra_doc_links)]
 
-extern crate alloc;
 #[cfg(feature = "prove")]
 extern crate ff;
 #[cfg(not(feature="std"))]
